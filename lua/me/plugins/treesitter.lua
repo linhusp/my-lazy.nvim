62cd22aeb25ae01b23dc1871a9885da4194eb25b
linhusp <linhusp3@protonmail.com>
2024-01-23 20:39:59 +0700

    lock treesitter version

--- lua/me/plugins/treesitter.lua
+++ lua/me/plugins/treesitter.lua
@@ -2,6 +2,8 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        version = "v0.9.2",
+        pin = true,
         config = function()
             require("nvim-treesitter.configs").setup({
                 sync_install = false,
