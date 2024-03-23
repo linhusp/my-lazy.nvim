000cbc1f68a5b22cffd9dd60926d66a3b42e032e
linhusp <linhusp3@protonmail.com>
2024-03-23 18:34:21 +0700

    add beautysh

--- lua/me/plugins/none-ls.lua
+++ lua/me/plugins/none-ls.lua
@@ -10,6 +10,7 @@ return {
                 null_ls.builtins.formatting.isort,
                 null_ls.builtins.formatting.blue,
                 null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.beautysh,
             },
         })
         vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
