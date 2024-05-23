8ffc82939d9994a4649ff6455e86acbd6bd2f5f1
linhusp <linhusp3@protonmail.com>
2024-05-23 08:06:58 +0700

    migrate to manual lspconfig to prevent issue with rustaceanvim

--- lua/me/plugins/lsp.lua
+++ lua/me/plugins/lsp.lua
@@ -19,61 +19,41 @@ return {
         'williamboman/mason-lspconfig.nvim',
         config = function()
             require('mason-lspconfig').setup({
-                ensure_installed = { 'lua_ls', 'pylsp' },
-            })
-
-            local opts = { noremap = true, silent = true }
-            -- :h vim.diagnostic.*
-            vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, opts)
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-            vim.keymap.set('n', 'd]', vim.diagnostic.goto_next, opts)
-            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
-            local lsp_attach = function(_, bufnr)
-                -- enable default completion <C-x><C-o>
-                -- :h vim.lsp.*
-                local bufopts = { noremap = true, silent = true, buffer = bufnr }
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-                vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-                vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-                -- vim.keymap.set('n', '<space>f', function()
-                --     vim.lsp.buf.format({ async = true })
-                -- end, bufopts)
-            end
-
-            local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-            -- :h mason-lspconfig-automatic-server-setup
-            require('mason-lspconfig').setup_handlers({
-                function(server_name)
-                    require('lspconfig')[server_name].setup({
-                        on_attach = lsp_attach,
-                        capabilities = lsp_capabilities,
-                    })
-                end,
-                ['lua_ls'] = function()
-                    require('lspconfig').lua_ls.setup({
-                        on_attach = lsp_attach,
-                        capabilities = lsp_capabilities,
-                        settings = {
-                            Lua = { diagnostics = { globals = { 'vim' } } },
-                        },
-                    })
-                end,
-                ['rust_analyzer'] = function()
-                end,
+                ensure_installed = { 'lua_ls', 'pylsp', 'rust_analyzer' },
             })
         end,
     },
-    { 'neovim/nvim-lspconfig' },
+    {
+        'neovim/nvim-lspconfig',
+        config = function()
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = { diagnostics = { globals = { 'vim' } } },
+                },
+            })
+            lspconfig.pylsp.setup({})
+
+            local opts = { noremap = true, silent = true }
+            -- :h vim.diagnostic.*
+            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+            vim.keymap.set('n', 'd]', vim.diagnostic.goto_next, opts)
+            -- :h vim.lsp.*
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+            vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        end,
+    },
+    -- for better rust-analyzer and rustfmt support
     {
         'mrcjkb/rustaceanvim',
         version = '^4',
-        lazy = false,
+        ft = 'rust'
     },
 }
