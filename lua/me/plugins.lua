99b588ceb4ac2bd753ee9d6f7752d00c99b834a1
linhusp <linhusp3@protonmail.com>
2024-05-23 07:15:17 +0700

    clean up unused plugins

--- lua/me/plugins.lua
+++ lua/me/plugins.lua
@@ -13,19 +13,19 @@ return {
     },
 
     -- status line
-    {
-        'nvim-lualine/lualine.nvim',
-        config = function()
-            require('lualine').setup({
-                options = {
-                    icons_enabled = false,
-                    theme = 'auto',
-                    component_separators = { left = '|', right = '|' },
-                    section_separators = { left = '', right = '' },
-                }
-            })
-        end
-    },
+    -- {
+    --     'nvim-lualine/lualine.nvim',
+    --     config = function()
+    --         require('lualine').setup({
+    --             options = {
+    --                 icons_enabled = false,
+    --                 theme = 'auto',
+    --                 component_separators = { left = '|', right = '|' },
+    --                 section_separators = { left = '', right = '' },
+    --             }
+    --         })
+    --     end
+    -- },
 
     -- better python indent
     {
@@ -36,82 +36,77 @@ return {
         end
     },
 
+    -- mini project
+    {
+        'echasnovski/mini.indentscope',
+        version = '*',
+        config = function()
+            local custom_animation = require('mini.indentscope').gen_animation.none()
+            require('mini.indentscope').setup({
+                draw = {
+                    delay = 0,
+                    animation = custom_animation,
+                },
+                symbol = '┆'
+            })
+        end
+    },
+    -- { 'echasnovski/mini.statusline', version = '*',   opts = {} },
+    { 'echasnovski/mini.pairs',      version = '*',   opts = {} },
+
     -- qol
-    {
-        "windwp/nvim-autopairs",
-        config = function()
-            require("nvim-autopairs").setup({
-                fast_wrap = {
-                    map = "<A-a>",
-                },
-                disable_in_macro = true,
-                disable_in_visualblock = true,
-                enable_check_bracket_line = false,
-                enable_afterquote = false,
-                map_cr = true,
-                check_ts = true,
-                ts_config = {
-                    javascript = { "template_string" },
-                },
-            })
-        end,
-    },
+    -- {
+    --     "windwp/nvim-autopairs",
+    --     config = function()
+    --         require("nvim-autopairs").setup({
+    --             fast_wrap = {
+    --                 map = "<A-a>",
+    --             },
+    --             disable_in_macro = true,
+    --             disable_in_visualblock = true,
+    --             enable_check_bracket_line = false,
+    --             enable_afterquote = false,
+    --             map_cr = true,
+    --             check_ts = true,
+    --             ts_config = {
+    --                 javascript = { "template_string" },
+    --             },
+    --         })
+    --     end,
+    -- },
 
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        config = function()
-            require("ibl").setup({
-                indent = {
-                    char = "┆",
-                },
-                scope = {
-                    show_start = false,
-                    show_end = false,
-                },
-                whitespace = {
-                    remove_blankline_trail = false,
-                },
-                exclude = {
-                    buftypes = { "terminal" },
-                },
-            })
-        end,
-    },
-
-    {
-        'lewis6991/gitsigns.nvim',
-        tag = 'release',
-        opts = {},
-    },
+    { 'lewis6991/gitsigns.nvim',     tag = 'release', opts = {}, },
 
     {
         'NvChad/nvim-colorizer.lua',
-        config = function()
-            require('colorizer').setup({
-                filetypes = {
-                    '*',
-                    '!python',
-                    css = { rgb_fn = true },
-                    html = { names = false }
-                },
-                user_default_options = {
-                    mode = 'background',
-                }
-            })
-        end,
-    },
-
-    {
-        'gsuuon/note.nvim',
         opts = {
-            spaces = { -- note roots, contains ./notes dir
-                '~/Desktop',
+            filetypes = {
+                '*',
+                '!python',
+                css = { rgb_fn = true },
+                html = { names = false }
             },
-            keymap = {
-                prefix = '<leader>n'
+            user_default_options = {
+                mode = 'background',
             }
         },
-        cmd = 'Note',
-        ft = 'note'
-    }
+    },
+
+    -- move code to the middle of the screen
+    {
+        'shortcuts/no-neck-pain.nvim',
+        version = '*',
+        opts = {
+            width = 100,
+            buffer = {
+                blend = -0.1,
+            }
+        },
+    },
+
+    -- additional UI progress message
+    {
+        'j-hui/fidget.nvim',
+        opts = {},
+    },
 }
